f9df725764e8f6986c43f646b7214bad514e2fb1
msitu22 <112602900+msitu22@users.noreply.github.com>
2023-09-24 21:23:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// // http web server
-// // Need to create secret key and certificate
-
-// // const hostname = 'localhost';
-// const port = 8080;
-// const https = require('https');
-// const express = require('express');
-// app = express();
-
-// const fs = require('fs');
-// // 3 ways to get the certificate
-// // 1. from public service
-// // 2. certificate authority
-// // 3. let's encrypt
-// const httpsOptions = {
-//     key: fs.readFileSync('key.pem'),
-//     cert: fs.readFileSync('cert.pem')
-// };
-// const server = https.createServer(app);
-
-// app.get('/', (req, res) => {
-//   res.send("Hello World")
-// });
-
-// server.listen(port, () => {
-//   console.log(`HTTPS is running on port` + port + `...`);
-// });
-
-
 //  this is a https web-server
 const https = require('https');
 const fs = require('fs');
@@ -45,13 +16,7 @@ app = express();
 const server = https.createServer(httpsOptions,app);
 
 app.use(express.json());
-// app.use('/', startUp)
 app.use('/', alive)
-
-// app.get('/get-ip', (req,res) => {
-//   const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-//   res.send({userIp});
-// })
 
 server.listen(port, () => {
     console.log('HTTPS is running on port ' + port + '')
